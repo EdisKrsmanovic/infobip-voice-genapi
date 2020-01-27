bb50777165357ff02f107445488f6dd125d41be4
Edis Krsmanovic <Edis.Krsmanovic@infobip.com>
2020-01-27 11:54:55 +0100

    Hotfix

--- infobip-voice-genapi-service/src/main/java/org/infobip/voice/genapi/validator/EndpointValidator.java
+++ infobip-voice-genapi-service/src/main/java/org/infobip/voice/genapi/validator/EndpointValidator.java
@@ -3,6 +3,7 @@ package org.infobip.voice.genapi.validator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.infobip.voice.genapi.connector.model.Endpoint;
 import org.infobip.voice.genapi.connector.model.ScenarioEndpoint;
+import org.infobip.voice.genapi.connector.model.SingleResponseEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -18,8 +19,8 @@ public class EndpointValidator {
     @Autowired
     private Validator validator;
 
-    public boolean checkIfValid(@Valid Endpoint endpoint) {
-        return endpoint.getResponse() == null || isJSONValid(endpoint.getResponse().getBody());
+    public boolean checkIfValid(@Valid SingleResponseEndpoint singleResponseEndpoint) {
+        return singleResponseEndpoint.getResponse() == null || isJSONValid(singleResponseEndpoint.getResponse().getBody());
     }
 
     public boolean checkIfValidScenario(@Valid ScenarioEndpoint scenarioEndpoint) {
